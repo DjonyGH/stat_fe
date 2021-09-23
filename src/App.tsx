3ada15baa1d41efc9293d37435dcb535f2875f29
Evgeniy Kalimullin <counsil1986@gmail.com>
2021-09-23 23:27:48 +0700

    added spiner

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,26 @@
-import { Button } from 'antd'
 import React, { FC, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import './App.css'
 import { useTypedSelector } from './hooks/useTypedSelector'
 import { issuersActionCreator } from './store/reducers/issuers/action-creators'
-import { AutoComplete } from 'antd'
 import { TIssuer } from './store/reducers/issuers/types'
+import { Button, AutoComplete, Spin } from 'antd'
 
 const { Option } = AutoComplete
 
 const App: FC = () => {
   const {
     issuers,
-    selectedIssuer
-    // isLoading,
+    selectedIssuer,
+    isLoading
     // error
   } = useTypedSelector((state) => state.issuersReducer)
   const dispatch = useDispatch()
   console.log('selectedIssuer', selectedIssuer)
 
   useEffect(() => {
-    console.log('useEffect')
     dispatch(issuersActionCreator.fetchIssuers())
-  }, [])
+  }, []) //eslint-disable-line
 
   const handleClickBtn = () => {
     console.log('Click')
@@ -32,24 +30,26 @@ const App: FC = () => {
     dispatch(issuersActionCreator.setSelectedIssuer({ id: option.key, name: value }))
 
   return (
-    <div className="App">
-      <Button style={{ marginTop: 20 }} type="primary" onClick={handleClickBtn}>
-        Primary Button
-      </Button>
-      <AutoComplete
-        style={{ width: 200, marginTop: 20 }}
-        placeholder="Выберите..."
-        onSelect={selectIssuer}
-        filterOption={true}
-        defaultActiveFirstOption={true}
-      >
-        {issuers.map((issuer: TIssuer) => (
-          <Option key={issuer.id} value={issuer.name}>
-            {issuer.name}
-          </Option>
-        ))}
-      </AutoComplete>
-    </div>
+    <Spin spinning={isLoading}>
+      <div className="App">
+        <Button style={{ marginTop: 20 }} type="primary" onClick={handleClickBtn}>
+          Primary Button
+        </Button>
+        <AutoComplete
+          style={{ width: 200, marginTop: 20 }}
+          placeholder="Выберите..."
+          onSelect={selectIssuer}
+          filterOption={true}
+          defaultActiveFirstOption={true}
+        >
+          {issuers.map((issuer: TIssuer) => (
+            <Option key={issuer.id} value={issuer.name}>
+              {issuer.name}
+            </Option>
+          ))}
+        </AutoComplete>
+      </div>
+    </Spin>
   )
 }
 
